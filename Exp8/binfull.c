0047d7cd0359155ede8112009682837a116bb223
brion4 <bharsathsatheesan@gmail.com>
2023-07-26 23:23:31 +0530

    modified:   Exp8/binfull.c

--- Exp8/binfull.c
+++ Exp8/binfull.c
@@ -2,16 +2,24 @@
 
 // Function to implement First Fit algorithm for bin packing
 int firstFit(int weights[], int n, int capacity) {
-    int bins = 0; 
-    int bin_capacity = capacity;
+    int bins = 0,j; 
+    int bin_capacity[10] = {10};
     
     for (int i = 0; i < n; i++) {
-        if (weights[i] <= bin_capacity) {
-            bin_capacity -= weights[i];
-        } else {
-            bins++;
-            bin_capacity = capacity - weights[i];
+        int flag=0;
+        for(j=0;j<bins;j++) {
+            if (weights[i] <= bin_capacity[j]) {
+                bin_capacity[j] -= weights[i];
+                flag=1;
+                break;
+            }
         }
+
+        if(flag==0) {
+            bins++;
+            bin_capacity[j] = capacity - weights[i];
+            }
+        
     }
     return bins;
 }
@@ -35,32 +43,32 @@ int nextFit(int weights[], int n, int capacity) {
 // Function to implement Best Fit algorithm for bin packing
 int bestFit(int weights[], int n, int capacity) {
     int bins = 0;
-    int bin_remaining_capacity[n];
+    int bin_rem_cp[n];
     
     for (int i = 0; i < n; i++) {
         int j;
-        int min_remaining_capacity = capacity + 1;
+        int min_rem_cp = capacity + 1;
         int best_bin = -1;
         
         for (j = 0; j < bins; j++) {
-            if (bin_remaining_capacity[j] >= weights[i] && bin_remaining_capacity[j] - weights[i] < min_remaining_capacity) {
+            if (bin_rem_cp[j] >= weights[i] && bin_rem_cp[j] - weights[i] < min_rem_cp) {
                 best_bin = j;
-                min_remaining_capacity = bin_remaining_capacity[j] - weights[i];
+                min_rem_cp = bin_rem_cp[j] - weights[i];
             }
         }
         
         if (best_bin == -1) {
-            bin_remaining_capacity[bins] = capacity - weights[i];
+            bin_rem_cp[bins] = capacity - weights[i];
             bins++;
         } else {
-            bin_remaining_capacity[best_bin] -= weights[i];
+            bin_rem_cp[best_bin] -= weights[i];
         }
     }
     return bins;
 }
 
 int main() {
-    int weights[] = {5, 7, 5, 2, 4, 2, 5, 1, 6};
+    int weights[] = {4, 1, 8, 1, 4, 2};
     int n = sizeof(weights) / sizeof(weights[0]);
     int bin_capacity = 10;
 
